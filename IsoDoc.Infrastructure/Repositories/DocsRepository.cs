27ff20d5a62a10a8e7d85a476078957d9a8efa33
Edalat <edalatfeizi921@gmail.com>
2025-04-10 09:47:40 +0330

    fix: improved filter docs query

--- IsoDoc.Infrastructure/Repositories/DocsRepository.cs
+++ IsoDoc.Infrastructure/Repositories/DocsRepository.cs
@@ -25,11 +25,11 @@ namespace IsoDoc.Infrastructure.Repositories
                 connection.Open();
 
                 if (isAdmin) {
-                    query = $"SELECT   ROW_NUMBER() OVER (ORDER BY RevisionDate DESC) AS RowId, * FROM  (select DISTINCT  * from VwAllDocument where LTRIM(RTRIM(DocumentName)) Like '%{searchTerm}%' or DocumentCode like '%{searchTerm}%'   AND (IsoDocViewID = 1)) as SubQuery ";
+                    query = $"SELECT   ROW_NUMBER() OVER (ORDER BY RevisionDate DESC) AS RowId, * FROM  (select DISTINCT  * from VwAllDocument where DocTypeID not in(0,6) and LTRIM(RTRIM(DocumentName)) Like '%{searchTerm}%' or DocumentCode like '%{searchTerm}%'   AND (IsoDocViewID = 1)) as SubQuery ";
                 }
                 else
                 {
-                    query = $"SELECT   ROW_NUMBER() OVER (ORDER BY RevisionDate DESC) AS RowId, * FROM  (select DISTINCT  * from VwAllDocument where (MdepartID ='{userDepId}' or mdepartid='PublicTechnicalDocuments') and  LTRIM(RTRIM(DocumentName)) Like '%{searchTerm}%' or DocumentCode like '%{searchTerm}%'   AND (IsoDocViewID = 1) and  IsShowPublic=1) as SubQuery";
+                    query = $"SELECT   ROW_NUMBER() OVER (ORDER BY RevisionDate DESC) AS RowId, * FROM  (select DISTINCT  * from VwAllDocument where (MdepartID ='{userDepId}' or mdepartid='PublicTechnicalDocuments') and DocTypeID not in(0,6) and LTRIM(RTRIM(DocumentName)) Like '%{searchTerm}%' or DocumentCode like '%{searchTerm}%'   AND (IsoDocViewID = 1) and  IsShowPublic=1) as SubQuery";
 
                 }
                 var docs = await connection.QueryAsync<SearchResult>(query);
